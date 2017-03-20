53a631525316def5668fc5e2f4e5c5759fc7adff
Tom Howard <tom@windyroad.com.au>
2017-03-20 18:03:31 +1100

    System test for getting a ryvr in the collection

--- src/main/java/au/com/mountainpass/ryvr/model/RyvrsCollection.java
+++ src/main/java/au/com/mountainpass/ryvr/model/RyvrsCollection.java
@@ -1,11 +1,19 @@
 package au.com.mountainpass.ryvr.model;
 
+import static de.otto.edison.hal.Link.*;
+import static de.otto.edison.hal.Links.*;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RyvrsCollection extends MutableHalRepresentation {
 
     private static final String TITLE = "Ryvrs";
+
+    public RyvrsCollection() {
+        super(linkingTo(
+                linkBuilder("self", "/ryvrs").withTitle("Ryvrs").build()));
+    }
 
     public int getCount() {
         return super.getEmbedded().getItemsBy("item").size();
