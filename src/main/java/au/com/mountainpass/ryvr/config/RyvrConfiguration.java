96428db12b6b280c24cb43fe6159e1ee6989dfd7
Tom Howard <tom@windyroad.com.au>
2017-04-20 19:28:30 +1000

    cleanup

--- src/main/java/au/com/mountainpass/ryvr/config/RyvrConfiguration.java
+++ src/main/java/au/com/mountainpass/ryvr/config/RyvrConfiguration.java
@@ -2,8 +2,6 @@ package au.com.mountainpass.ryvr.config;
 
 import java.net.URI;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
 import org.springframework.context.ApplicationListener;
@@ -15,25 +13,8 @@ public class RyvrConfiguration implements
 
     private int port;
 
-    @Value("${server.ssl.key-store}")
-    private String keyStore;
-
-    @Value("${server.ssl.key-store-password}")
-    private String keyStorePassword;
-
-    @Value("${server.ssl.key-password}")
-    private String keyPassword;
-
-    @Value("${server.ssl.key-alias}")
-    private String keyAlias;
-
     @Value("${au.com.windyroad.service-gateway.ssl.hostname}")
     private String sslHostname;
-
-    @Value("${javax.net.ssl.trustStore:build/truststore.jks}")
-    private String trustStoreFile;
-
-    public final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     public int getPort() {
         return port;
