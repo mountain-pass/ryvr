e111bf86cde34c3c5cc0648870b2b70dd98c8727
Tom Howard <tom@windyroad.com.au>
2017-05-23 06:39:16 +1000

    Fixed file names

--- src/test/java/au/com/mountainpass/ryvr/testclient/RyvrTestJarServerProcessBuilder.java
+++ src/test/java/au/com/mountainpass/ryvr/testclient/RyvrTestJarServerProcessBuilder.java
@@ -1,18 +1,29 @@
 package au.com.mountainpass.ryvr.testclient;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import cucumber.api.PendingException;
-
 @Component
-@Profile(value = { "dockerRun" })
+@Profile(value = { "jarRun" })
 public class RyvrTestJarServerProcessBuilder
         implements RyvrTestServerProcessBuilder {
 
     @Override
     public ProcessBuilder getProcessBuilder(String configLocation) {
-        throw new PendingException("TODO");
+        ProcessBuilder setupPb = new ProcessBuilder("bash", "./gradlew",
+                "bootRepackage").inheritIO();
+        try {
+            Process setupProcess = setupPb.start();
+            setupProcess.waitFor(30, TimeUnit.SECONDS);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return new ProcessBuilder("java", "-jar",
+                "build/libs/ryvr-1.0.0-SNAPSHOT.jar",
+                "--spring.config.location=" + configLocation);
     }
 
 }
