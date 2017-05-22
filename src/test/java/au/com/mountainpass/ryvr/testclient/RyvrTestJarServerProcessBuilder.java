c3c801a428840535b87ee9b6a6d32b4423f25718
Tom Howard <tom@windyroad.com.au>
2017-05-23 01:08:09 +1000

    Added System test using Repackaged Jar

---
+++ src/test/java/au/com/mountainpass/ryvr/testclient/RyvrTestJarServerProcessBuilder.java
@@ -0,0 +1,29 @@
+package au.com.mountainpass.ryvr.testclient;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile(value = { "jarRun" })
+public class RyvrTestJarServerProcessBuilder
+        implements RyvrTestServerProcessBuilder {
+
+    @Override
+    public ProcessBuilder getProcessBuilder(String configLocation) {
+        ProcessBuilder setupPb = new ProcessBuilder("bash", "./gradlew",
+                "bootRepackage").inheritIO();
+        try {
+            Process setupProcess = setupPb.start();
+            setupProcess.waitFor(30, TimeUnit.SECONDS);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return new ProcessBuilder("java", "-jar",
+                "build/libs/ryvr-1.0.0-SNAPSHOT.jar",
+                "--spring.config.location=" + configLocation);
+    }
+
+}
