3e1492f5ff19947edb86610724b9606aa72543a2
Tom Howard <tom@windyroad.com.au>
2017-07-28 07:42:04 +1000

    cleanup

---
+++ src/test/java/au/com/mountainpass/ryvr/RyvrTests_System_DistZipRun_Rest_MySqlLocal.java
@@ -0,0 +1,16 @@
+package au.com.mountainpass.ryvr;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+
+import au.com.mountainpass.SpringProfileCucumber;
+import cucumber.api.CucumberOptions;
+
+@RunWith(SpringProfileCucumber.class)
+@CucumberOptions(plugin = { "pretty" }, features = {
+    "src/test/resources/features/" }, strict = false, glue = {
+        "au.com.mountainpass.ryvr" }, tags = { "~@performance" })
+@ActiveProfiles({ "systemTest", "distZipRun", "restApi", "mysql" })
+public class RyvrTests_System_DistZipRun_Rest_MySqlLocal {
+
+}
